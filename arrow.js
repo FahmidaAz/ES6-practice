577345f2ed5ee539118064422335826fa2f88ae0
FahmidaAz <66080543+FahmidaAz@users.noreply.github.com>
2021-08-06 13:29:12 -0400

    file add

---
+++ arrow.js
@@ -0,0 +1,17 @@
+function doubleIt(num){
+    return num*2;
+}
+
+const result = doubleIt(200);
+console.log(result);
+
+const trippleIt = function(num){
+     return num*3; 
+}
+
+const  result2 = trippleIt(7);
+console.log(result2);
+
+const tripple = number => number*3;
+const result3 = tripple(12);
+console.log(result3);
