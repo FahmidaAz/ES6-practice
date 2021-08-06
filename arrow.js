1e216344e21112571adda95dc407631709b44265
FahmidaAz <66080543+FahmidaAz@users.noreply.github.com>
2021-08-06 14:11:56 -0400

    thirdFile done

--- arrow.js
+++ arrow.js
@@ -15,3 +15,21 @@ console.log(result2);
 const tripple = number => number*3;
 const result3 = tripple(12);
 console.log(result3);
+
+
+const add = (x,y) => x+y;
+const result4 = add(10,20);
+console.log(result4);
+
+const noReturn = () => console.log("Hi there!!");
+const result5 = noReturn();
+
+const complicate = (num1, num2) =>{
+    const sum = num1 + num2;
+    const diff = num1-num2;
+    const result = sum * diff;
+
+    return result;
+}
+const result6 = complicate(12,10);
+console.log(result6);
