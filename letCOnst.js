e2734b6e88a11adc56fddea53496c8897d4ea2c3
FahmidaAz <66080543+FahmidaAz@users.noreply.github.com>
2021-08-06 12:55:59 -0400

    first file

---
+++ letCOnst.js
@@ -0,0 +1,13 @@
+const hubby = "Kazi Faruk";
+//hubby = "Tinku";
+let boyFrnd = "Kuddus ali";
+boyFrnd = "Murgi mia";
+console.log (boyFrnd);
+
+function add (a,b = 20){
+
+    return a+b;
+
+ }
+ const result =  add(5,10);
+console.log(result);
