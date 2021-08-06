2bf5866f54b341f4eb12ce0a1d8b0ea00c83ffc4
FahmidaAz <66080543+FahmidaAz@users.noreply.github.com>
2021-08-06 13:11:37 -0400

    second file

---
+++ template.js
@@ -0,0 +1,21 @@
+const firstName = "Shoily";
+const lastName ="Azmin";
+const fullName = firstName +" "+  lastName+ " is a good girl";
+const fullName2 = `${firstName} ${lastName} is a good girl`;
+console.log(fullName2);
+
+const multiLine = "Lorem ipsum dolor sit amet\n" +"consectetur adipisicing elit.\n" +"Nemo temporibus ullam minima, unde illo sint at tempora!\n" +" Officiis blanditiis, libero ipsam corrupti alias, perferendis, repellat\n" +"voluptate reiciendis repudiandae quibusdam quas!";
+
+console.log(multiLine);
+
+
+//ES6 multiline
+const multiline2 = `Lorem ipsum, dolor sit amet consectetur 
+adipisicing elit.
+ Dolor necessitatibus, ullam maiores
+  sapiente id soluta. 
+ Rerum, id laudantium, incidunt odio vel,
+  eius quam officiis illo
+  quisquam magni pariatur neque hic.`;
+  
+  console.log(multiline2);
